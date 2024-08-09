5fb9d03f2ef2f345822f8d66168e79453d4c9535
AshanJR01 <111280972+AshanJR01@users.noreply.github.com>
2024-08-09 08:16:05 +0530

    filter-page-default-values-set

--- src/app/pages/filter/filter.page.ts
+++ src/app/pages/filter/filter.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.page.scss'],
 })
 export class FilterPage implements OnInit {
+  filters = {
+    meal_type: 'lunch',
+    cuisine: 'indian',
+    distance: 5,
+    popular: true,
+    priceByOrder: 'highToLow',
+    price: { lower: 5, upper: 10 },
+  };
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  mealTypeChange(ev: any) {
+    console.log(ev.detail.value);
+    const type = ev.detail.value;
+    this.filters.meal_type = type;
   }
-
 }
